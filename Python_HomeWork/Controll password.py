d4f9c6e11614f9e97a4a0156d1b9a6c1b671f98d
Vitalik <75811667+lkdrm@users.noreply.github.com>
2022-01-19 20:18:04 +0100

    Update Controll password.py

--- Python_HomeWork/Controll password.py
+++ Python_HomeWork/Controll password.py
@@ -10,8 +10,9 @@ def user():
         key = input("Enter a login: ")
         if key in dict_of_keys:
             value = input("Enter a password: ")
-            if value in dict_of_keys[key]:
+            if value == dict_of_keys[key]:
                 print("You are - log in")
+                return True
             else:
                 count+=1
                 print("Enter a correct password")
@@ -19,7 +20,7 @@ def user():
             print("Enter a correct login")
         if count == 3:
             print("You have write wrong passwords we block your acess!")
-            break
+            return False
  
 
 user()
