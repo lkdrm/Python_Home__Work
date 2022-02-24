f5bdf8b22d9ca4e4e20b05b35ba44f42fa0070f9
Vitalik <75811667+lkdrm@users.noreply.github.com>
2022-02-24 16:48:43 +0100

    Add files via upload

--- Home_Work__files/5 task/home_work_module7_part1.py
+++ Home_Work__files/5 task/home_work_module7_part1.py
@@ -2,8 +2,23 @@
 # You have a text file. Calculate the number of words that
 # begin with a character set by the user.
 
+import re
+"""
 with open("fifth_text.txt","r") as reader:
     sum_of_leters = []
     for word in reader:
         sum_of_leters.append(len(word))
     print(r"The sum of letters are:",sum(sum_of_leters))
+"""
+
+regex_compile = re.compile(r"[d]")
+
+with open ("fifth_text.txt", "r") as reader:
+    sum_of_leters = []
+    for letter in reader:
+        regex_search = regex_compile.findall(letter)
+        if len(regex_search) > 0 :
+            #print(regex_search)
+            for index in regex_search:
+                sum_of_leters.append(index)
+    print("The sum of my letters are:",len(sum_of_leters))
