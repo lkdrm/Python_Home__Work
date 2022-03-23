e64c71b731060623886a3fbaa291be65d1683f86
Vitalik <75811667+lkdrm@users.noreply.github.com>
2022-03-23 18:27:46 +0100

    Add files via upload

---
+++ about__numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class MyNumpy:
+    def __init__(self):
+        pass
+
+    def arange(number):
+        return np.arange(1,number+1)
---
+++ about_matplot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from about__numpy import MyNumpy as my_lst
+
+my_list = my_lst
+my_list_of_numbers = my_list.arange(20)
+my_list_of_numbers_2 = my_list_of_numbers + 1
+my_list_of_numbers_3 = my_list_of_numbers + 2
+my_list_of_numbers_4 = my_list_of_numbers + 3
+my_list_of_numbers_5 = my_list_of_numbers + 4
+my_list_of_numbers_6 = my_list_of_numbers + 5
+numbers = range(1,len(my_list_of_numbers)+1)
+
+print(numbers)
+fig,ax = plt.subplots(figsize = (11,6))
+ax.plot(numbers,my_list_of_numbers,   lw = 1, ls = "solid",   c = "red", label = "normal",)
+ax.plot(numbers,my_list_of_numbers_2, lw = 2, ls = "dotted",  c = "green", label = "count+1",)
+ax.plot(numbers,my_list_of_numbers_3, lw = 3, ls = "dashed",  c = "blue", label = "count+2",)
+ax.plot(numbers,my_list_of_numbers_4, lw = 4, ls = "dashdot", c = "pink", label = "count+3",)
+ax.plot(numbers,my_list_of_numbers_5, lw = 5, ls = "dotted",  c = "yellow", label = "count+4",)
+ax.plot(numbers,my_list_of_numbers_6, lw = 6, ls = "solid",   c = "black", label = "count+5",)
+
+plt.show()
+
